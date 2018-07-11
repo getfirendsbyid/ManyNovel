b378fa6b7521a789d02b96599bb4642d3c054559
yuwei <1094692718@qq.com>
2018-07-11 11:21:37 +0800

    sd

--- resources/views/janpa/show.blade.php
+++ resources/views/janpa/show.blade.php
@@ -61,7 +61,7 @@
             <!--广告-->
             <!--//广告-->
             <p class="text-center readPager">
-                {{dd($last_chapter)}}
+                {{dd(empty($last_chapter))}}
                 @if(empty($befor_chapter))
                 <a class="btn btn-default" href="/"><i class="fa fa-arrow-circle-left fa-fw"></i>没有啦</a>
                 @else
