55c2b9ea195d15002f0100531e82a26539561806
yuwei <1094692718@qq.com>
2018-07-17 14:23:58 +0800

    s

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,15 @@ Route::get('sitemap.xml','HomeController@sitemap'); //网站sitemap
 Route::get('home','NovelController@index'); //队列工具
 
 Route::domain('www.'.checkhost())->group(function () {
-    Route::middleware(['CountSpider','cacheResponse:5'])->get('/','HomeController@index');
+    Route::middleware('CountSpider')->get('/','HomeController@index');
 });
 
 Route::domain('zhannei.'.checkhost())->group(function () {
-    Route::middleware(['CountSpider','cacheResponse:5'])->get('/search','HomeController@search');
+    Route::middleware('CountSpider')->get('/search','HomeController@search');
 });
 
 Route::domain('{account}.'.checkhost())->group(function ($account) {
-    Route::middleware(['CountSpider','cacheResponse:5'])->get('/','HomeController@fan');
+    Route::middleware('CountSpider')->get('/','HomeController@fan');
 });
 
 Route::get('/book/{bookname}','HomeController@chapterlist');
